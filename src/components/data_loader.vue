09f94a9a286752aa42c6173cf76488a23df24128
akleinau <56925445+akleinau@users.noreply.github.com>
2025-07-18 18:15:50 +0200

    make ready for pages

--- src/components/data_loader.vue
+++ src/components/data_loader.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 
 import * as d3 from 'd3'
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
 import {useDataStore} from "../stores/dataStore.ts";
 
 const files = ref(null)
@@ -16,11 +16,27 @@ function uploaded(files: any) {
   }
   reader.readAsText(csvFile)
 }
+
+const load_dataset = async () => {
+  const csvFile = "https://raw.githubusercontent.com/akleinau/SoliEventsMD/refs/heads/main/dataset/SoliEventsMD.csv"
+  const data = await d3.csv(csvFile, {crossOrigin: "anonymous"})
+  if (!data || data.columns.length <= 3) {
+    console.error("No data found online. Switch to manual upload.")
+    return
+  }
+  dataStore.set_data(data)
+}
+
+onMounted(async () => {
+  await load_dataset()
+})
+
+
 </script>
 
 <template>
   <v-file-input label="Input the csv here" v-model="files"
-                accept=".csv" v-if="files == null"
+                accept=".csv" v-if="dataStore.data == null"
                 @update:modelValue="uploaded"></v-file-input>
 </template>
 
