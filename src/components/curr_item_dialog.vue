5fdd4c63f163d9b44bc5a0ed70752996586b68a3
jomino151 <jonas.morawietz@proton.me>
2026-01-20 15:08:56 +0100

    improved styling

--- src/components/curr_item_dialog.vue
+++ src/components/curr_item_dialog.vue
@@ -69,30 +69,34 @@ const showWerbegrafik = computed(() => {
         <span>{{ item.Was }}</span>
         <v-icon size="x-large" color="black" class="dialog-title__icon">{{ dataStore.getCategoryIcon(item.Kategorie) }}</v-icon>
       </v-card-title>
-      <v-card-subtitle>{{ item.Wer }}</v-card-subtitle>
       <v-card-text>
-        <!-- Werbegrafik image -->
-        <v-img
-            v-if="showWerbegrafik"
-            :src="werbegrafikPath!"
-            class="werbegrafik-image mb-4"
-            max-height="400"
-            contain
-            @error="handleWerbegrafikError"
-        >
-          <template v-slot:placeholder>
-            <v-row class="fill-height ma-0" align="center" justify="center">
-              <v-progress-circular indeterminate color="grey-lighten-5"></v-progress-circular>
-            </v-row>
-          </template>
-        </v-img>
+        <v-row>
+          <v-col cols="12" :md="showWerbegrafik ? 7 : 12">
+            <p class="mb-3 text-subtitle-1 text-medium-emphasis">{{ item.Wer }}</p>
+            <p class="mb-1"> <v-icon>mdi-map-marker</v-icon> {{ item.Wo }}</p>
+            <p class="mb-1"> <v-icon>mdi-calendar</v-icon> {{ dataStore.getFormattedDay(item.Wochentag ?? '') }}, {{ item.Rhythmus }}</p>
+            <p class="mb-1"> <v-icon>mdi-clock</v-icon> {{ item.Uhrzeit_Start }} - {{ item.Uhrzeit_Ende }}</p>
+            <p class="mt-5">
+              <a :href="item.Link" target="_blank">{{ item.Link }}</a>
+            </p>
+          </v-col>
 
-        <p class="mb-1"> <v-icon>mdi-map-marker</v-icon> {{ item.Wo }}</p>
-        <p class="mb-1"> <v-icon>mdi-calendar</v-icon> {{ dataStore.getFormattedDay(item.Wochentag ?? '') }}, {{ item.Rhythmus }}</p>
-        <p class="mb-1"> <v-icon>mdi-clock</v-icon> {{ item.Uhrzeit_Start }} - {{ item.Uhrzeit_Ende }}</p>
-        <p class="mt-5">
-          <a :href="item.Link" target="_blank">{{ item.Link }}</a>
-        </p>
+          <v-col v-if="showWerbegrafik" cols="12" md="5">
+            <v-img
+                :src="werbegrafikPath!"
+                class="werbegrafik-image"
+                max-height="300"
+                contain
+                @error="handleWerbegrafikError"
+            >
+              <template v-slot:placeholder>
+                <v-row class="fill-height ma-0" align="center" justify="center">
+                  <v-progress-circular indeterminate color="grey-lighten-5"></v-progress-circular>
+                </v-row>
+              </template>
+            </v-img>
+          </v-col>
+        </v-row>
 
         <p
             v-if="!isVerificationStale"
@@ -109,7 +113,6 @@ const showWerbegrafik = computed(() => {
         >
           {{ verificationWarning ?? 'Achtung!' }}
         </v-alert>
-
       </v-card-text>
     </v-card>
   </v-dialog>
